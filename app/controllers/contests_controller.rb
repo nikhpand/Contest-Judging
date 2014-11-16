0177dfb92d8d9287a7ef0895d92fb7fce1b14c95
akash-tyagi <akash.t@tamu.edu>
2014-11-16 11:14:38 -0600

    Adding Basic Contest Model/Views

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -2,4 +2,27 @@ class ContestsController < ApplicationController
     def index
         @contests = Contest.all
     end
+    
+    def show
+        id = params[:id]
+        @contest = Contest.find(id)
+    end
+    
+    def new 
+        @contest = Contest.new
+    end
+    
+    def create 
+        @contest = Contest.new(contest_params)
+        render 'new'
+        if @contest.save
+            #Success
+        else
+            render 'new'
+        end
+    end
+    
+    def contest_params
+        params.require(:contest).permit(:name, :location, :date)
+    end
 end
