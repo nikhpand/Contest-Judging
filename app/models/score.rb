8db386f51b63a37c9b53ef1989136422ebfc8ecd
Nikhilesh Pandey <nikhpand@tamu.edu>
2014-12-13 09:22:12 +0000

    UI changes

--- app/models/score.rb
+++ app/models/score.rb
@@ -29,13 +29,23 @@ class Score < ActiveRecord::Base
        user.save!
    end
    
-   def self.to_csv(options = {})
-       CSV.generate(options) do |csv|
-           csv << column_names
-           all.each do |score|
-               csv << score.attributes.values_at(*column_names)
-           end
-       end
-   end
+#   def self.to_csv(options = {})
+#       CSV.generate(options) do |csv|
+#           csv << column_names
+#           all.each do |score|
+#               csv << score.attributes.values_at(*column_names)
+#           end
+#       end
+#   
+
+  def Score.to_csv(scores=[], options = {})
+      CSV.generate(options) do |csv|
+          csv << ['Round Number','Project Name','Username','Question','Score','Comments']
+          scores.each do |score|
+              csv << [score.round_number,score.project_name,score.user_name,score.question_name,score.score,score.question_comment]
+              #score.attributes.values_at(*scores.column_names)
+          end
+      end
+  end
 
 end
